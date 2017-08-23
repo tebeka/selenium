a7e71178aa40d9b298d464b912c4e831b2be4455
Eric Garrido <ekg@google.com>
2017-08-23 10:22:33 -0400

    Speed up Travis testing: move dependency downloading
    Travis tests are timing out. Move downloading dependencies out of the
    'script' portion of a Travis run.
    
    Issue #95.

--- testing/travis-ci-test.sh
+++ testing/travis-ci-test.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 # Run tests under Travis for continuous integration.
 
-go get -d -v
-pushd vendor
-go get -d -v
-go run init.go --alsologtostderr --download_browsers
-popd
 go test -coverprofile=coverage.txt -covermode=atomic -test.v -timeout=20m \
   --start_frame_buffer=false --test.run=TestFirefoxSelenium3
